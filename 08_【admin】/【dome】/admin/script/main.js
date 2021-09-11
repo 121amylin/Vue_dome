ba3f38e40c3f96bda2113f2f21bdc90b7ae68f40
121amylin <121amylin@gmail.com>
2021-09-11 19:52:40 +0800

    validation、admin切版

---
+++ 08_【admin】/【dome】/admin/script/main.js
@@ -0,0 +1,119 @@
+new Vue({
+  el: '#admin',
+  data() {
+    return {
+
+      //表格資訊
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1517 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1519 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1516 弄',
+        zip: 200333
+      }],
+      //新增欄位
+      ruleForm: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      rules: {
+        name: [{
+            required: true,
+            message: '请输入活动名称',
+            trigger: 'blur'
+          },
+          {
+            min: 3,
+            max: 5,
+            message: '长度在 3 到 5 个字符',
+            trigger: 'blur'
+          }
+        ],
+        region: [{
+          required: true,
+          message: '请选择活动区域',
+          trigger: 'change'
+        }],
+        date1: [{
+          type: 'date',
+          required: true,
+          message: '请选择日期',
+          trigger: 'change'
+        }],
+        date2: [{
+          type: 'date',
+          required: true,
+          message: '请选择时间',
+          trigger: 'change'
+        }],
+        type: [{
+          type: 'array',
+          required: true,
+          message: '请至少选择一个活动性质',
+          trigger: 'change'
+        }],
+        resource: [{
+          required: true,
+          message: '请选择活动资源',
+          trigger: 'change'
+        }],
+        desc: [{
+          required: true,
+          message: '请填写活动形式',
+          trigger: 'blur'
+        }]
+      }
+
+    }
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+
+})
