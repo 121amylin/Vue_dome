612905b9be97c84f432facc29f38ddc13cc8746a
121amylin <121amylin@gmail.com>
2021-09-13 21:56:40 +0800

    API revised

--- 04_【filter】/filter/src/App.vue
+++ 04_【filter】/filter/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app" v-if="isloadend">
-
     <myheader></myheader>
     <div class="main">
       <div class="container flex">
         <myaside></myaside>
-        <div id="content"><router-view /></div>
+        <!-- <div id="content"><router-view /></div> -->
+        {{ get_10 }}
       </div>
     </div>
   </div>
@@ -24,10 +24,23 @@ export default {
   methods: {},
   created () {
     this.$store.dispatch('get_API')
+    this.$store.dispatch('get_API_group')
   },
   mounted () {
     if (this.$store.state) {
       this.isloadend = true
+    }
+  },
+  computed: {
+    get_data () {
+      return this.$store.state.db2
+    },
+    get_10 () {
+      const arr = []
+      for (let i = 0; i < 10; i++) {
+        arr.push(this.get_data[i])
+      }
+      return arr
     }
   }
 }
--- 04_【filter】/filter/src/store/index.js
+++ 04_【filter】/filter/src/store/index.js
@@ -7,9 +7,23 @@ Vue.use(VueAxios, axios)
 
 Vue.use(Vuex)
 
+function API (url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(res => {
+        // eslint-disable-next-line prefer-promise-reject-errors
+        return res.status === 200 ? res.json() : reject()
+      })
+      .then(result => {
+        resolve(result)
+      })
+  })
+}
+
 export default new Vuex.Store({
   state: {
     db: null,
+    db2: null,
     now_city: '臺中市',
     first_data: 0
   },
@@ -17,13 +31,14 @@ export default new Vuex.Store({
     GET_DB (state, date) {
       state.db = date
     },
+    GET_DB2 (state, date) {
+      state.db2 = date
+    },
     CHANGE_CITY (state, date) {
       state.now_city = date
     }
   },
-  getters: {
-
-  },
+  getters: {},
   actions: {
     get_API (store) {
       axios
@@ -33,6 +48,57 @@ export default new Vuex.Store({
         .then(res => {
           store.commit('GET_DB', res.data)
         })
+    },
+    get_API_group (store) {
+      const temp = []
+      const url1 =
+        'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=1'
+      const url2 =
+        'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=2'
+      const url3 =
+        'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=3'
+      const url4 =
+        'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=4'
+      const url5 =
+        'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=5'
+      const url6 =
+        'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6'
+
+      async function apiGroup () {
+        const arr = [
+          API(url1),
+          API(url2),
+          API(url3),
+          API(url4),
+          API(url5),
+          API(url6)
+        ]
+        for (const item of arr) {
+          await item
+            .then(res => {
+              return res.map(item => {
+                return temp.push({
+                  UID: item.UID,
+                  title: item.title,
+                  location: item.showInfo[0].location,
+                  showUnit: item.showUnit,
+                  descriptionFilterHtml: item.descriptionFilterHtml,
+                  webSales: item.webSales,
+                  imageUrl: item.imageUrl,
+                  startDate: item.startDate,
+                  endDate: item.endDate
+                })
+              })
+            })
+            .catch(err => {
+              console.log(err)
+            })
+        }
+        console.log(temp)
+        store.commit('GET_DB2', temp)
+      }
+
+      apiGroup()
     }
   },
   modules: {}
